35f267b1e483849000e594587b57fd19fb738aec
Masudphero~ <mdmasudrana8547@gmail.com>
2025-01-07 21:41:43 +0600

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <div className='top-strip py-2'>
+                <div className='container'>
+                    <div className='flex items-center justify-between'>
+                         <div className='coll w-[50%]'>
+                          <p className='text-[14px]'>Get up to 50% off season styles,limited time only</p>
+                         </div>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
