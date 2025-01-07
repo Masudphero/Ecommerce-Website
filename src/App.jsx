2455f5dedecdda8963fd26c84aec466c1441e614
Masudphero~ <mdmasudrana8547@gmail.com>
2025-01-07 22:59:48 +0600

    heder text

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import Home from './pages/Home/Home'
 
 function App() {
   
 
   return (
     <>
+      <BrowserRouter>
       <Header></Header>
+      <Routes>
+        <Route path={'/'} exact={true} element={<Home></Home>}>
+          
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,29 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
-            <div className='top-strip py-2'>
+            <div className='top-strip py-2 border mt-1 border-y-grey-700'>
                 <div className='container'>
                     <div className='flex items-center justify-between'>
                          <div className='coll w-[50%]'>
-                          <p className='text-[14px]'>Get up to 50% off season styles,limited time only</p>
+                          <p className='text-[14px] font-semibold'>Get up to 50% off season styles,limited time only</p>
                          </div>
+
+                          
+                          <div className='col2 flex items-center justify-end'>
+                            <ul>
+                                <li className='list-none'>
+                                <Link>Help Center</Link>
+                                </li>
+                            </ul>
+
+                          </div>
+
+
+
                     </div>
                 </div>
 
